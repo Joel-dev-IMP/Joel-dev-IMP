97fd452acab981e51f26c0ec82279e138ed080a9
Joel <77357583+Joel-dev-IMP@users.noreply.github.com>
2026-09-25 11:32:13 +0200

    refactor: Move the list of tools and skills to 'tools_and_skills.csv'
    This enables easier editing and shortens unnecessary parts and changes in the README.md

--- tools_and_skills.sql
+++ tools_and_skills.sql
@@ -6,54 +6,15 @@ CREATE TABLE tools_and_skills (
 );
 
 INSERT INTO
-    tools_and_skills (name, category, last_used, comment)
-VALUES
-    (
-        'Python',
-        'PROGRAMMING_LANGUAGES',
-        '2026-09-20',
-        'First programming language'
-    ),
-    (
-        'Javascript',
-        'PROGRAMMING_LANGUAGES',
-        '2026-09-16',
-        ''
-    ),
-    ('C', 'PROGRAMMING_LANGUAGES', '2026-01-17', ''),
-    ('Java', 'PROGRAMMING_LANGUAGES', '2025-12-18', ''),
-    (
-        'OCaml',
-        'PROGRAMMING_LANGUAGES',
-        '2025-08-13',
-        'Learned this at TUM'
-    ),
-    ('C++', 'PROGRAMMING_LANGUAGES', '2025-06-18', ''),
-    ('Markdown', 'DOCUMENTS', '2026-09-19', ''),
-    (
-        'Typst',
-        'DOCUMENTS',
-        '2026-09-19',
-        'Check out https://typst.app/'
-    ),
-    ('LaTeX', 'DOCUMENTS', '2026-07-13', ''),
-    ('HTML', 'WEB_DEVELOPMENT', '2026-09-16', ''),
-    ('CSS', 'WEB_DEVELOPMENT', '2026-09-12', ''),
-    ('Typescript', 'WEB_DEVELOPMENT', '2026-09-12', ''),
-    ('Tailwind', 'WEB_DEVELOPMENT', '2026-09-12', ''),
-    ('Next.js', 'WEB_DEVELOPMENT', '2026-09-20', ''),
-    ('Figma', 'DESIGN_TOOLS', '2026-09-10', ''),
-    (
-        'GIMP',
-        'GRAPHICS_EDITORS',
-        '2026-09-15',
-        'I hope the GIMP UX gets a little love'
-    ),
-    ('Docker', 'CONTAINERS', '2026-09-20', ''),
-    ('SQL', 'DATABASES', '2026-09-20', ''),
-    ('Bash', 'COMMAND_LINES', '2026-09-16', ''),
-    ('Git', 'VERSION_CONTROL', '2026-09-20', ''),
-    ('Visual Studio Code', 'IDE', '2026-09-20', '');
+    tools_and_skills (name, category, last_used, comment) (
+        SELECT
+            name,
+            category,
+            last_used,
+            comment
+        FROM
+            'tools_and_skills.csv'
+    );
 
 SELECT
     name,
